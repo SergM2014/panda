a2f2394ffa2a51bbe8b62aeb04c456d3a8d5999d
SergM2014 <weisse011@gmail.com>
2025-07-08 23:42:46 +0300

    routes correction

--- routes.php
+++ routes.php
@@ -1,20 +1,20 @@
 <?php
 
-Get('/', [ \Src\Actions\User::class, 'index']);
-Get('/register', [ \Src\Actions\User::class, 'registerForm']);
-Post('/register', [ \Src\Actions\User::class, 'addUser']);
-Get('/login', [ \Src\Actions\User::class, 'loginForm']);
-Post('/login', [ \Src\Actions\User::class, 'login']);
-Get('/logout', [ \Src\Actions\User::class, 'logout']);
+Get(key: '/', arg: [ \Src\Actions\User::class, 'index']);
+Get(key: '/register', arg: [ \Src\Actions\User::class, 'registerForm']);
+Post(key: '/register', arg: [ \Src\Actions\User::class, 'addUser']);
+Get(key: '/login', arg: [ \Src\Actions\User::class, 'loginForm']);
+Post(key: '/login', arg: [ \Src\Actions\User::class, 'login']);
+Get(key: '/logout', arg: [ \Src\Actions\User::class, 'logout']);
 
-Get('/admin', [ \Src\Actions\Survey::class, 'index']);
-Get('/admin/survey/create', [ \Src\Actions\Survey::class, 'create']);
-Post('/admin/survey/store', [ \Src\Actions\Survey::class, 'store']);
-Get('/admin/user/survey', [ \Src\Actions\Survey::class, 'allByUser']);
-Post('/admin/survey/delete', [ \Src\Actions\Survey::class, 'delete']);
-Get('/admin/survey/edit', [ \Src\Actions\Survey::class, 'edit']);
-Post('/admin/survey/update', [ \Src\Actions\Survey::class, 'update']);
+Get(key: '/admin', arg: [ \Src\Actions\Survey::class, 'index']);
+Get(key: '/admin/survey/create', arg: [ \Src\Actions\Survey::class, 'create']);
+Post(key: '/admin/survey/store', arg: [ \Src\Actions\Survey::class, 'store']);
+Get(key: '/admin/user/survey', arg: [ \Src\Actions\Survey::class, 'allByUser']);
+Post(key: '/admin/survey/delete', arg: [ \Src\Actions\Survey::class, 'delete']);
+Get(key: '/admin/survey/edit', arg: [ \Src\Actions\Survey::class, 'edit']);
+Post(key: '/admin/survey/update', arg: [ \Src\Actions\Survey::class, 'update']);
 
-Any('/api/surveys', [ \Src\Actions\API::class, 'all']);
-Any('/api/survey/{argument}', [ \Src\Actions\API::class, 'get']);
-Any('/api/user/{argument}/survey', [ \Src\Actions\API::class, 'getByUserId']);
+Any(key: '/api/surveys', arg: [ \Src\Actions\API::class, 'all']);
+Any(key: '/api/survey/{argument}', arg: [ \Src\Actions\API::class, 'get']);
+Any(key: '/api/user/{argument}/survey', arg: [ \Src\Actions\API::class, 'getByUserId']);
