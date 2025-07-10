5ac39934cacc318d296d4223466ca31c30601d0c
SergM2014 <weisse011@gmail.com>
2025-07-10 19:35:05 +0300

    psr api ammendments

--- src/Actions/API.php
+++ src/Actions/API.php
@@ -12,16 +12,16 @@ class API
 
     public function all(): void
     {
-        echo json_encode($this->repository->all());
+        echo json_encode(value: $this->repository->all());
     }
 
     public function get(mixed $id): void
     {
-        echo json_encode($this->repository->get((int)$id));
+        echo json_encode(value: $this->repository->get(id: (int)$id));
     }
 
     public function getByUserId(int $id): void
     {
-        echo json_encode($this->repository->getByUserId($id));
+        echo json_encode(value: $this->repository->getByUserId(id: $id));
     }
 }
